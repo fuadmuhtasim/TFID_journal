1f87c2701e63389ea4135c47ec6dfb7a55822192
Fuad <jeet0001@umn.edu>
2024-06-07 20:35:17 -0700

    Initial commit

---
+++ static/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('show-signup').addEventListener('click', function() {
+        document.getElementById('signup-screen').classList.remove('hidden');
+        document.getElementById('login-screen').classList.add('hidden');
+    });
+
+    document.getElementById('show-login').addEventListener('click', function() {
+        document.getElementById('signup-screen').classList.add('hidden');
+        document.getElementById('login-screen').classList.remove('hidden');
+    });
+});
+
