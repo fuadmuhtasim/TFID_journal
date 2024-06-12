a332c2cf5c17c4b7503b361443f338b1ccf2c3e6
fuadmuhtasim <fuadmuhtasim@gmail.com>
2024-06-11 20:01:06 -0700

    Figured out how to serve index page using Actix

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ async fn greet() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            .service(actix_files::Files::new("/", "./static").index_file("index.html"))
             .route("/", web::get().to(greet)) // Configure the route
     })
     .bind("127.0.0.1:8080")? // Bind to an address and port
