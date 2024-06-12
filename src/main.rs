3d9d18198ef0f89ddfb0210f645d8d7ff75257f0
fuadmuhtasim <fuadmuhtasim@gmail.com>
2024-06-11 18:48:58 -0700

    Added some new dependencies. Trying to figure out actix at the moment and actors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+// Define a handler function
+async fn greet() -> impl Responder {
+    HttpResponse::Ok().body("Hello, Actix!")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet)) // Configure the route
+    })
+    .bind("127.0.0.1:8080")? // Bind to an address and port
+    .run()
+    .await
 }
