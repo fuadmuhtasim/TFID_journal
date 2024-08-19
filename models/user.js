6091b20e1584f36b271f062c20cfa318f3f06dc1
fuadmuhtasim <jeetfuad@gmail.com>
2024-08-18 17:11:30 -0700

    Some more files added

--- models/user.js
+++ models/user.js
@@ -6,25 +6,6 @@ class User {
         this.password = password;
         this.type = type;
     }
-    //function to save user to database
-//     async saveToDatabase() {
-//         //hashes password
-
-//         //connects to database using pool object
-//         const client = await pool.connect();
-//             //if cannot connect return error
-//         //checks if same email already exists
-//             //returns error "save email already exists"
-//         //using query, and pool object, save the User to the database
-//     }
-//     //function to retrieve user from database
-//         //hashes password
-//         //connects to database using pool object
-//             //if cannot connect return error
-//         //checks if email and password matches
-//             //returns error "email password incorrect"
-//             //otherwise, if no error, updates the User details and assumes current user for 
-//                 //further operations
 }
 
 
