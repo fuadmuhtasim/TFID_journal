6b4fcfb0b04b8dabfbe6657e6e1470e3110b926b
fuadmuhtasim <jeetfuad@gmail.com>
2024-10-19 15:22:12 -0500

    Added CORS

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const signupRoute = require("./routes/signup");
 const add = require("./routes/add");
 const { cookieJwtAuth } = require("./middleware/cookieJwtAuth");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 
 //Serve static files from the 'public' directory using virtual "hidden" path
@@ -16,6 +17,9 @@ app.use('/hidden', express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 //To encode and decode form email data in form
 app.use(cookieParser());
+//Requiring cross origin resource sharing
+app.use(cors());
+app.use(express.json());
 
 //Serve the index.html file on the root path
 app.get('/', (req, res) => {
@@ -34,7 +38,23 @@ app.post('/login_form', loginRoute);
 //Handler: Add Submission Endpoint
 app.post('/add', cookieJwtAuth, add);
 
+//CONTENT FOR USERS
+  //ROUTES WITH POSTGRES QUERIES
+
+  //Create journal entry
+  
+  //Get a journal entry
+
+  //Update a journal entry
+
+  //Delete a journal entry
+
+
+
+
 //Listen to the port ->
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`)
 })
+
+
